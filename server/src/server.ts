96c7148a56a42b79e33c23c76aff4eebc5946865
Thiago Negreiros <negreiros.t@gmail.com>
2023-02-03 10:14:27 -0300

    feat(main): create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors)
-
-app.get('/', () => {
-  return 'Hello World!'
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3334
